3fa6a14ca267debee2fc72c9330dca14a2d85fd1
baihua <zhangbohan_wx@sjtu.edu.cn>
2024-05-29 00:57:17 +0800

    Test.

--- BackEnd/main.cpp
+++ BackEnd/main.cpp
@@ -3,9 +3,8 @@
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    std::cin.tie(nullptr);
-    freopen("../testcases/TicketSystemData/data/17.in", "r", stdin);
-    freopen("../testcases/TicketSystemData/ans.out", "w", stdout);
+//    freopen("../testcases/TicketSystemData/data/17.in", "r", stdin);
+//    freopen("../testcases/TicketSystemData/ans.out", "w", stdout);
     ticket::System system("");
     std::string cmd;
     while (std::getline(std::cin, cmd)) {
